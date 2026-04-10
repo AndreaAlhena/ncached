f1ef0905dedbabcc4ac61747f6239609ec71a4c1
Andrea Alhena <andrea.tantimonaco@outlook.com>
2026-04-10 22:21:32 +0200

    feat: export all new public API symbols

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,23 @@
  * Public API Surface of ncached
  */
 
+// Compressors
+export * from './lib/compressors/lz-string.compressor';
+export * from './lib/compressors/noop.compressor';
+
+// Interfaces
+export * from './lib/interfaces/cache-entry.interface';
 export * from './lib/interfaces/cache-object.interface';
+export * from './lib/interfaces/cache-observable-options.interface';
+export * from './lib/interfaces/compressor.interface';
+export * from './lib/interfaces/ncached-config.interface';
+export * from './lib/interfaces/set-options.interface';
+
+// Namespaces
 export * from './lib/namespaces/cache-service-errors.namespace';
+
+// Services
 export * from './lib/services/ncached.service';
+
+// Tokens
+export * from './lib/tokens/ncached-config.token';
